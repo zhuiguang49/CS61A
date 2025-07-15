7c38a017ae74d1430c4eabd03fd1a35c82321c49
zhuiguang <3230105121@zju.edu.cn>
2025-07-15 14:46:53 +0800

    finish hw01 and lab01

---
+++ lab/lab01/lab01.py
@@ -0,0 +1,76 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    count=0
+    result = 1
+    while count < k:
+        result *= n
+        n-=1
+        count+=1
+
+    return result
+
+
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    sum = 0
+    temp = 0
+    while y > 0:
+        temp = y%10
+        sum = sum +temp
+        y = y //10
+    return sum
+
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    temp = n
+    pre = temp % 10
+    temp = temp //10
+    while temp > 0:
+        cur = temp % 10
+        if cur == 8 and pre == 8:
+            return True
+        pre = cur
+        temp = temp //10
+        
+    return False
+
